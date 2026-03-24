24c200855931c24be024f1585e376fd027789fa4
Jingyu Liu <381258337@qq.com>
2026-03-24 20:50:41 +0800

    update sbatch

---
+++ sbatch/typeII_2d_rand/typeII_2d_rand.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=typeII_2d_rand
+#SBATCH --output=typeII_2d_rand_%j.out
+#SBATCH --error=typeII_2d_rand_%j.err
+#SBATCH --nodelist=bigMem0
+#SBATCH --time=18:00:00
+#SBATCH --cpus-per-task=16
+
+export PATH=$PATH:/home/jyliu/NUFFT/extern/fftw-3.3.10/build_double/bin:/home/jyliu/NUFFT/extern/fftw-3.3.10/build_single/bin
+export LIBRARY_PATH=$LIBRARY_PATH:/home/jyliu/NUFFT/extern/fftw-3.3.10/build_double/lib:/home/jyliu/NUFFT/extern/fftw-3.3.10/build_single/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jyliu/NUFFT/extern/fftw-3.3.10/build_double/lib:/home/jyliu/NUFFT/extern/fftw-3.3.10/build_single/lib
+export CPATH=$CPATH:/home/jyliu/NUFFT/extern/fftw-3.3.10/build_double/include:/home/jyliu/NUFFT/extern/fftw-3.3.10/build_single/include
+
+module unload MATLAB
+module load MATLAB/R2023b
+matlab -r 'cd /home/jyliu/NUFFT2D; nufft2d_startup; cd experiments/typeII_2d_rand; exp_typeII_2d_rand;'
