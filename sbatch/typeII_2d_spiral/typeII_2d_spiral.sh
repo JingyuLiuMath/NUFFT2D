84463a0a4da844ca2be607a4a062d7bee60e617c
JingyuLiu <381258337@qq.com>
2026-03-30 10:27:43 +0100

    update test

---
+++ sbatch/typeII_2d_spiral/typeII_2d_spiral.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=spiral
+#SBATCH --output=spiral_%j.out
+#SBATCH --error=spiral_%j.err
+#SBATCH --nodelist=bigMem0
+#SBATCH --time=18:00:00
+#SBATCH --exclusive
+
+export PATH=$PATH:/home/jyliu/NUFFT2D/extern/fftw-3.3.10/build_double/bin:/home/jyliu/NUFFT2D/extern/fftw-3.3.10/build_single/bin
+export LIBRARY_PATH=$LIBRARY_PATH:/home/jyliu/NUFFT2D/extern/fftw-3.3.10/build_double/lib:/home/jyliu/NUFFT2D/extern/fftw-3.3.10/build_single/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jyliu/NUFFT2D/extern/fftw-3.3.10/build_double/lib:/home/jyliu/NUFFT2D/extern/fftw-3.3.10/build_single/lib
+export CPATH=$CPATH:/home/jyliu/NUFFT2D/extern/fftw-3.3.10/build_double/include:/home/jyliu/NUFFT2D/extern/fftw-3.3.10/build_single/include
+
+module unload MATLAB
+module load MATLAB/R2023b
+matlab -r 'cd /home/jyliu/NUFFT2D; nufft2d_startup; cd experiments/typeII_2d_spiral; exp_typeII_2d_spiral;'
